ce6e64a653aaca71f390e0a7eeff2992b83a6a20
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-14 22:36:26 +0530

    Added API For Movies

--- backend/src/main/java/com/movie/booking/backend/movie_booking_backend/config/configration.java
+++
@@ -1,4 +0,0 @@
-package com.movie.booking.backend.movie_booking_backend.config;
-
-public class configration {
-}
---
+++ backend/src/main/java/com/movie/booking/backend/movie_booking_backend/controller/MovieController.java
@@ -0,0 +1,27 @@
+package com.movie.booking.backend.movie_booking_backend.controller;
+
+
+import com.movie.booking.backend.movie_booking_backend.model.Movie;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Controller
+@RequestMapping("/api/movies")
+public class MovieController {
+    @GetMapping
+
+    public List<Movie> getAllMovies(){
+        return  List.of(
+                Movie.builder().title("Animal").language("Hindi").genre("Action").build(),
+                Movie.builder().title("Interstellar").language("English").genre("Si-fi").build(),
+                Movie.builder().title("3 Idiots").language("Hindi").genre("Comedy").build()
+        );
+    }
+
+
+}
