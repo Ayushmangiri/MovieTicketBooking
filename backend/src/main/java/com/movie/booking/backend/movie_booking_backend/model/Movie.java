8c5eb3de3c5799c1e39be8ed06a17434e48076e4
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-14 22:11:02 +0530

    Added config

---
+++ backend/src/main/java/com/movie/booking/backend/movie_booking_backend/model/Movie.java
@@ -0,0 +1,17 @@
+package com.movie.booking.backend.movie_booking_backend.model;
+
+import lombok.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+
+public class Movie {
+    private String title;
+    private  String language;
+    private String genre;
+}
