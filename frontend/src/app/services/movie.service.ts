95bcc422e52f9dd80580a37a93946652224968cb
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-15 11:56:43 +0530

    Added frontend component

--- frontend/src/app/services/movie.service.ts
+++ frontend/src/app/services/movie.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '../module'; // Correct import
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>('http://localhost:8080/api/movies');
+  }
 }
