5bc704229ef6005614e90df505764da4c4d094a4
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-18 22:08:31 +0530

    Added

--- backend/src/main/java/com/movie/booking/backend/movie_booking_backend/service/CustomerService.java
+++ backend/src/main/java/com/movie/booking/backend/movie_booking_backend/service/CustomerService.java
@@ -1,10 +1,21 @@
 package com.movie.booking.backend.movie_booking_backend.service;
 
+import com.movie.booking.backend.movie_booking_backend.model.Customer;
 import com.movie.booking.backend.movie_booking_backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class CustomerService {
 
     @Autowired
     CustomerRepository customerRepository;
+
+
+    public Customer saveCustomer(Customer customer){
+        return  customerRepository.save(customer);
+    }
+    public List<Customer>getAllCustomer(){
+        return customerRepository.findAll();
+    }
 }
