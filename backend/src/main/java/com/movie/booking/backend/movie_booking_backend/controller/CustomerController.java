097db3b79a1385828f2a45e45ee912cfc57c9afe
Ayushmangiri <ayushmangiri@gmail.com>
2025-06-21 21:21:20 +0530

    Added Feature

--- backend/src/main/java/com/movie/booking/backend/movie_booking_backend/controller/CustomerController.java
+++ backend/src/main/java/com/movie/booking/backend/movie_booking_backend/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package com.movie.booking.backend.movie_booking_backend.controller;
 
+import com.movie.booking.backend.movie_booking_backend.model.Customer;
+import com.movie.booking.backend.movie_booking_backend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+
+    @Autowired
+    public CustomerService customerService;
+
+    @PostMapping
+    public Customer createCustomer (@RequestBody Customer customer){
+        return  customerService.saveCustomer(customer);
+    }
 }
